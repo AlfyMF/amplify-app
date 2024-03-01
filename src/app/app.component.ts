84e6c6fce0d220926a44133c6ebbf642074840ab
AlfyMF <alfy.francis@experionglobal.com>
2024-03-01 11:45:42 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TodosComponent,
+    AmplifyAuthenticatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
